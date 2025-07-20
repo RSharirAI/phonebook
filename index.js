f31a5794bb71989d9f36f4384d81043fd9611260
RSharirAI <Sharir_learning@proton.me>
2025-07-20 11:31:56 +0300

    mongo + render connections

--- index.js
+++ index.js
@@ -1,83 +1,56 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const app = express()
-const cors = require('cors')
+const Person = require('./models/person')
 
+const app = express()
 
 app.use (express.json())
 app.use(express.static('dist'))
-// app.use(morgan('tiny'))
 morgan.token('post-body', (req,res) => {
     if (req.method === 'POST') {
         return JSON.stringify(req.body)
     }
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
-app.use(cors())
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
 ]
 
-const generateId = () => {
-  const timestamp = Date.now() // Current time in milliseconds
-  const random = Math.floor(Math.random() * 10000) // Random number 0-9999
-  return `${timestamp}${random}`
-
-}
-
 app.get ('/api/persons', (request, response) => {
-    response.json(persons)    
+    Person.find({})
+        .then(persons => {
+            response.json(persons)
+        }
+        )    
 })
 
-app.get('/api/info', (request, response) => {
-    const currentTime = new Date().toString()
-    const personsCount = persons.length 
+// app.get('/api/info', (request, response) => {
+//     const currentTime = new Date().toString()
+//     const personsCount = persons.length 
     
-    const infoHtml = `
-        <div>
-            <p>Phonebook has info for ${personsCount} people</p>
-            <p>${currentTime}</p>
-        </div>
-    `
-    response.send(infoHtml)
-})
+//     const infoHtml = `
+//         <div>
+//             <p>Phonebook has info for ${personsCount} people</p>
+//             <p>${currentTime}</p>
+//         </div>
+//     `
+//     response.send(infoHtml)
+// })
 
 app.get ('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-
-    if (person) 
-        response.json(person)
-    else
-        response.status(404).end()
-    
+    Person.findByID(request.params.id)
+        .then(person => {
+            response.json(person)
+        })
 })
 
 app.delete ('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
+    Person.findByIdAndDelete(request.params.id)
+    // const id = request.params.id
+    // persons = persons.filter(person => person.id !== id)
 
-    response.status(204).end()
+    // response.status(204).end()
 })
 
 app.post ('/api/persons', (request, response) => {
@@ -108,18 +81,18 @@ app.post ('/api/persons', (request, response) => {
         })
     }
     
-    const person = {
-        id: generateId(),
+    const person = new Person({
         name: body.name,
         number: body.number
-        
-    }
+    })
 
-    persons = persons.concat(person)
-    response.json(person)
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
